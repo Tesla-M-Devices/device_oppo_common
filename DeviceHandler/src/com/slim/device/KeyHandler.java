dadb6ca588254f5f01a94e349ec1da16f54364c9
Martinusbe <martinusbe@gmail.com>
2017-08-15 11:03:42 +0200

    slider: fix all notifications
     * removed that whitespace
    
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- DeviceHandler/src/com/slim/device/KeyHandler.java
+++ DeviceHandler/src/com/slim/device/KeyHandler.java
@@ -35,6 +35,8 @@ import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
 
+import android.service.notification.ZenModeConfig;
+
 import com.slim.device.settings.ScreenOffGesture;
 
 import com.android.internal.os.DeviceKeyHandler;
@@ -164,6 +166,7 @@ public class KeyHandler implements DeviceKeyHandler {
                 break;
             case MODE_NONE:
                 setZenMode(Settings.Global.ZEN_MODE_OFF);
+                setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
                 break;
             case MODE_VIBRATE:
                 setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
@@ -172,7 +175,7 @@ public class KeyHandler implements DeviceKeyHandler {
                 setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
                 break;
             }
-            
+
             if (action == null || action != null && action.equals(ActionConstants.ACTION_NULL)) {
                 return;
             }
